c80160fc4835adcc78fa9ae81eba72b22a9d0578
krysiuk93 <ivan.krysiuk.93@gmail.com>
2022-06-27 04:41:46 -0300

    pre-muestra

--- arcana/aplicaciones/rituales/models.py
+++ arcana/aplicaciones/rituales/models.py
@@ -1,3 +1,22 @@
+from tabnanny import verbose
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+class Ritual(models.Model):
+    id = models.AutoField(primary_key = True)
+    titulo = models.CharField('Título', max_length = 90, blank = False, null = False)
+    slug = models.CharField('Slug', max_length = 100, blank = False, null = False)
+    descripcion = models.CharField('Descripción', max_length = 110, blank = False, null = False)
+    imagen = models.URLField(max_length = 255, blank = False, null= False)
+    contenido = RichTextField()
+    estado = models.BooleanField('Publicado/No Publicado', default = True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now = False, auto_now_add = True)
+
+    class Meta:
+        verbose_name = 'Ritual'
+        verbose_name_plural = 'Rituales'
+
+    def __str__(self):
+        return self.titulo
--- arcana/aplicaciones/rituales/views.py
+++ arcana/aplicaciones/rituales/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Ritual
 
 # Create your views here.
 
 def rituales(request):
-    return render(request, 'rituales.html')
+    ritual = Ritual.objects.filter(estado = True)
+    return render(request, 'rituales.html',{'ritual':ritual})
 
 def ritual(request):
     return render(request, 'ritual.html')
